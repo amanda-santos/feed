2fe550a25d37d20e625428ef57ebd4d9319304ec
amanda-santos <amandasf0200@gmail.com>
2022-07-14 19:32:07 -0300

    feat: ✨ add header

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from "react";
+
+import { Header } from "./components";
+
+import "./global.css";
+
+export const App = (): ReactElement => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from "react";
+
+import styles from "./Header.module.css";
+import logo from "../../assets/logo.svg";
+
+export const Header = (): ReactElement => {
+  return (
+    <header className={styles.header}>
+      <img src={logo} alt="Logo" />
+    </header>
+  );
+};
